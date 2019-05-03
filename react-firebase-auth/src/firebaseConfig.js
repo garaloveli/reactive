1e0c506b5abd55a6e51741e3d9d5e78d96090c3a
Olga Zegarra Ordóñez <olga984@gmail.com>
2019-05-03 12:12:23 -0500

    cleaning

--- react-firebase-auth/src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: "AIzaSyBdJ3sFcoRi4bFvqcicR6e_luwGTHalaOE",
-    authDomain: "nyrie-web.firebaseapp.com",
-    databaseURL: "https://nyrie-web.firebaseio.com",
-    projectId: "nyrie-web",
-    appId: "1:774739941044:web:03044c14330ce3fa"
-  };
-
-  export default config;
